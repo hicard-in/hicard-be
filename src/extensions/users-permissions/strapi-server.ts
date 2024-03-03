23c98163c90b4ee2091bd3274c92a7e0fe4a1db5
vishal-pandey <contact@vishalpandey.co.in>
2024-03-03 09:54:50 +0000

    bug: connection refused fix

--- src/extensions/users-permissions/strapi-server.ts
+++ src/extensions/users-permissions/strapi-server.ts
@@ -12,11 +12,8 @@ const client = new S3Client({
 
 })
 module.exports = (plugin)=>{
-  console.log("CP1", plugin)
   plugin.controllers.user.updateMe = async (ctx) =>{
-    console.log("CP2", ctx)
     if(!ctx.state.user || !ctx.state.user.id){
-      console.log("INSIDE IF", ctx)
       return ctx.response.status = 401;
     }
     await strapi.entityService.update('plugin::users-permissions.user', ctx.state.user.id, {
@@ -24,7 +21,7 @@ module.exports = (plugin)=>{
     }).then(async (res)=>{
 
       const username = res.username;
-      let data = await axios.get(`http://localhost:1337/api/users/?filters[$or][0][username][$eq]=${username}&filters[$or][1][userId][$eq]=${username}&populate=deep`)
+      let data = await axios.get(`http://${host}:${port}/api/users/?filters[$or][0][username][$eq]=${username}&filters[$or][1][userId][$eq]=${username}&populate=deep`)
       data = data.data
       
       const usernameInput = {
