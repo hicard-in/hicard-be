f22b0b0b720b2d4507f0af71041bf2c765954de7
Vishal Pandey <contact@vishalpandey.co.in>
2024-06-21 19:34:23 +0000

    afterUpdate hook

--- src/index.ts
+++ src/index.ts
@@ -32,37 +32,44 @@ export default {
     strapi.db.lifecycles.subscribe({
       models: ['plugin::users-permissions.user'],
       async afterCreate(event) {
-        // Save new internal user id to database
-        let username = event?.result?.username
-
-        console.log("CHECKING USERNAME\n\n\n\n", username, "\n\n\n\n\n USERNAME")
-
-        let data = await axios.get(`http://${process.env.HOST}:${process.env.PORT}/api/users/?filters[$or][0][username][$eq]=${username}&filters[$or][1][userId][$eq]=${username}&populate=deep`)
-        data = data.data
-        
-        const usernameInput = {
-          "Body": JSON.stringify(data),
-          "Bucket": process.env.CF_BUCKET,
-          "Key": `api/${username}.json`,
-          "ContentType": "application/json",        
-        }
-        
-        const userId = data[0]?.userId
-        const userIdInput = {
-          "Body": JSON.stringify(data),
-          "Bucket": process.env.CF_BUCKET,
-          "Key": `api/${userId}.json`,
-          "ContentType": "application/json",        
-        }
-        
-        console.log(usernameInput, "CHECKING HEREE");
-        const usernameCommand = new PutObjectCommand(usernameInput)
-        const userIdCommand = new PutObjectCommand(userIdInput)
-
-        const usernameRes = await client.send(usernameCommand)
-        const userIdRes = await client.send(userIdCommand)
+        await pushToR3(event)
+        return true
+      },
+      async afterUpdate(event) {
+        await pushToR3(event)
         return true
       },
     });
   },
 };
+
+async function pushToR3(event) {
+  let username = event?.result?.username
+
+  console.log("CHECKING USERNAME\n\n\n\n", username, "\n\n\n\n\n USERNAME")
+
+  let data = await axios.get(`http://${process.env.HOST}:${process.env.PORT}/api/users/?filters[$or][0][username][$eq]=${username}&filters[$or][1][userId][$eq]=${username}&populate=deep`)
+  data = data.data
+  
+  const usernameInput = {
+    "Body": JSON.stringify(data),
+    "Bucket": process.env.CF_BUCKET,
+    "Key": `api/${username}.json`,
+    "ContentType": "application/json",        
+  }
+  
+  const userId = data[0]?.userId
+  const userIdInput = {
+    "Body": JSON.stringify(data),
+    "Bucket": process.env.CF_BUCKET,
+    "Key": `api/${userId}.json`,
+    "ContentType": "application/json",        
+  }
+  
+  console.log(usernameInput, "CHECKING HEREE");
+  const usernameCommand = new PutObjectCommand(usernameInput)
+  const userIdCommand = new PutObjectCommand(userIdInput)
+
+  const usernameRes = await client.send(usernameCommand)
+  const userIdRes = await client.send(userIdCommand)
+}
