7b6fab0100bdce972475a54432e55bed2522d5cf
Vishal Pandey <contact@vishalpandey.co.in>
2024-06-21 18:37:28 +0000

    after update hook

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,16 @@
+import { S3Client, ListBucketsCommand, PutObjectCommand } from "@aws-sdk/client-s3";
+import axios from 'axios';
+
+const client = new S3Client({
+  region: 'us-east-1',
+  credentials: {
+    accessKeyId: process.env.CF_ACCESS_KEY_ID,
+    secretAccessKey: process.env.CF_ACCESS_SECRET,
+  },
+  endpoint: process.env.CF_ENDPOINT_ABS
+
+})
+
 export default {
   /**
    * An asynchronous register function that runs before
@@ -14,5 +27,42 @@ export default {
    * This gives you an opportunity to set up your data model,
    * run jobs, or perform some special logic.
    */
-  bootstrap(/*{ strapi }*/) {},
+  bootstrap({ strapi }) {
+    
+    strapi.db.lifecycles.subscribe({
+      models: ['plugin::users-permissions.user'],
+      async afterCreate(event) {
+        // Save new internal user id to database
+        let username = event?.result?.username
+
+        console.log("CHECKING USERNAME\n\n\n\n", username, "\n\n\n\n\n USERNAME")
+
+        let data = await axios.get(`http://${process.env.HOST}:${process.env.PORT}/api/users/?filters[$or][0][username][$eq]=${username}&filters[$or][1][userId][$eq]=${username}&populate=deep`)
+        data = data.data
+        
+        const usernameInput = {
+          "Body": JSON.stringify(data),
+          "Bucket": process.env.CF_BUCKET,
+          "Key": `api/${username}.json`,
+          "ContentType": "application/json",        
+        }
+        
+        const userId = data[0]?.userId
+        const userIdInput = {
+          "Body": JSON.stringify(data),
+          "Bucket": process.env.CF_BUCKET,
+          "Key": `api/${userId}.json`,
+          "ContentType": "application/json",        
+        }
+        
+        console.log(usernameInput, "CHECKING HEREE");
+        const usernameCommand = new PutObjectCommand(usernameInput)
+        const userIdCommand = new PutObjectCommand(userIdInput)
+
+        const usernameRes = await client.send(usernameCommand)
+        const userIdRes = await client.send(userIdCommand)
+        return true
+      },
+    });
+  },
 };
